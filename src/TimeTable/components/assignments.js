f01e59f2a378139010db515c037404f628a92b99
GYMMICH\Thomas.Janke <thomas.janke@wgmail.de>
2019-01-17 14:50:19 +0100

    Add link to assignment

--- src/TimeTable/components/assignments.js
+++ src/TimeTable/components/assignments.js
@@ -6,6 +6,28 @@ import AssignmentIcon from '@material-ui/icons/Assignment';
 import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 
+function getAssignmentLink(assignment) {
+    // from Assignment notification bot
+    const subEntityId = JSON.stringify({
+        "version": "1.0",
+        "config": {
+            "classes": [
+                {
+                    "id": assignment.classId,
+                    "assignmentIds": [
+                        assignment.id
+                    ]
+                }
+            ]
+        },
+        "action": "navigate",
+        "view": "assignment-viewer"
+    });
+
+    return `https://teams.microsoft.com/l/entity/66aeee93-507d-479a-a3ef-8f494af43945/classroom?context=${JSON.stringify({ subEntityId })}`;
+
+}
+
 export default function Assignments(props) {
     return (
         <React.Fragment>
@@ -20,7 +42,7 @@ export default function Assignments(props) {
                     content.push(match[1]);
                 }
                 return (
-                    <ListItem button key={assignment.id}>
+                    <ListItem button key={assignment.id} component="a" href={getAssignmentLink(assignment)}>
                         <ListItemIcon>
                             {turnedIn ? <AssignmentTurnedInIcon /> : <AssignmentIcon />}
                         </ListItemIcon>
