e892e1c2409b7389809a5b6755528600ccbe32de
Michael Janke <mjanke@ebay.com>
2019-07-25 14:20:50 +0200

    upgrade and joined team in interval

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { loadJoinedTeams } from './actions';
 import TimeTable from '../TimeTable';
 import { intervalCheckStart, intervalCheckStop } from './intervalCheck';
 
 class Main extends Component {
     constructor(props) {
         super(props);
-        props.loadJoinedTeams();
         intervalCheckStart();
     }
 
@@ -19,18 +16,4 @@ class Main extends Component {
         intervalCheckStop();
     }
 }
-
-const mapDispatchToProps = dispatch => {
-    return {
-        loadJoinedTeams: () => dispatch(loadJoinedTeams()),
-    };
-};
-
-const mapStateToProps = state => {
-    return {};
-};
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Main);
+export default Main;
