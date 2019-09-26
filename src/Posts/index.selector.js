cea7d20dd4934dfef04de08fb77acbfdb52ee12e
TomiC <tomikeyke@gmail.com>
2019-09-27 01:55:36 +0200

    Filter expired posts

--- src/Posts/index.selector.js
+++ src/Posts/index.selector.js
@@ -14,6 +14,18 @@ export default function makeGetPosts() {
                 }
             }
             const newPosts = posts
+                .filter(post =>
+                    post.USER_CREATED
+                    || moment().isBetween(
+                        moment(post.DATE_FROM.date),
+                        moment(post.DATE_TO.date),
+                        'days',
+                        '[]'
+                    ))
+                // accept user_created
+                // check if post needs to be displayed
+                // important: this is calculated if posts reference changes and on reloading page
+                //             -> when post is "expiring" post is shown until page gets reloaded
                 .sort((post1, post2) =>
                     (post2.USER_CREATED - post1.USER_CREATED)
                     || moment(post2.DATE_FROM.date).diff(post1.DATE_FROM.date)
