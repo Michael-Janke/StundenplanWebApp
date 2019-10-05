3bc406142f3d14f230a21d813e6996ddf0e451b7
Michael Janke <micha@jankeweb.de>
2019-10-05 11:19:58 +0200

    avatar loading

--- src/Main/components/Avatars.js
+++ src/Main/components/Avatars.js
@@ -5,8 +5,7 @@ import ClassIcon from '@material-ui/icons/Group';
 import RoomIcon from '@material-ui/icons/Room';
 import SubjectIcon from '@material-ui/icons/Book';
 import debounce from 'debounce';
-import { makeGetEffectiveAvatar } from '../../Selector/avatars';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { loadAvatars } from '../actions';
 import { dispatch } from '../../store';
 
@@ -20,52 +19,39 @@ export const ObjectIcon = ({ type, upn, size, profilePicSize, outline, ...other 
     if (type === 'room' || type === 'all') {
         return <RoomIcon {...other} />;
     }
-    return <ProfilePicture {...{ upn, size, profilePicSize, outline, ...other }} />;
+    return <UserIcon {...{ upn, size, profilePicSize, outline }} {...other} />;
 };
 
 var loadUpns = [];
+
 const dispatchLoad = debounce(() => {
     dispatch(loadAvatars(loadUpns));
     loadUpns = [];
 }, 200);
-const load = upn => {
+
+const loadAvatar = upn => {
     loadUpns.push(upn);
     dispatchLoad();
 };
 
-const ProfilePictureComponent = ({
-    upn,
-    avatar,
-    size = 24,
-    profilePicSize,
-    outline = false,
-    loadAvatars,
-    ...other
-}) => {
-    upn && load(upn);
-    return avatar && avatar.img ? (
-        <Avatar
-            src={avatar.img}
-            {...(size && { style: { height: profilePicSize || size, width: profilePicSize || size } })}
-            {...other}
-        />
-    ) : outline ? (
-        <Avatar {...(size && { style: { height: size, width: size } })} {...other}>
-            <PersonIcon />
-        </Avatar>
-    ) : (
-        <PersonIcon {...(size && { style: { height: size, width: size } })} {...other} />
-    );
+export const UserIcon = ({ upn, size = 24, profilePicSize, outline, ...other }) => {
+    upn && loadAvatar(upn);
+    const avatar = useSelector(state => state.avatars[upn]);
+    if (avatar && avatar.img) {
+        return (
+            <Avatar
+                src={avatar.img}
+                style={{ height: profilePicSize || size, width: profilePicSize || size }}
+                {...other}
+            />
+        );
+    }
+    if (outline) {
+        return (
+            <Avatar style={{ height: size, width: size }} {...other}>
+                <PersonIcon />
+            </Avatar>
+        );
+    }
+    return <PersonIcon style={{ height: size, width: size }} {...other} />;
 };
-
-const makeMapStateToProps = () => {
-    const getEffectiveAvatar = makeGetEffectiveAvatar();
-    return (state, props) => ({
-        avatar: getEffectiveAvatar(state, props),
-    });
-};
-
-export const ProfilePicture = connect(
-    makeMapStateToProps,
-    { loadAvatars: upn => loadAvatars(upn) }
-)(ProfilePictureComponent);
--- src/Main/components/UserAvatar.js
+++ src/Main/components/UserAvatar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { ProfilePicture } from './Avatars';
+import { UserIcon } from './Avatars';
 
 const UserAvatar = ({ size }) => {
     const upn = useSelector(state => state.user.upn);
-    return <ProfilePicture upn={upn} size={size} outline={true} />;
+    return <UserIcon upn={upn} size={size} outline={true} />;
 };
 
 export default UserAvatar;
